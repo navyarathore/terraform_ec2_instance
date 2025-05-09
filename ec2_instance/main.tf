d887bb3c7902f6cdc101ce7582e32163f86f155d
Navya Rathore <rathorenavya45@gmail.com>
2025-05-09 08:35:49 +0530

    fix: minor changes

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,19 +1,3 @@
-# resource "aws_instance" "my_ec2_instance" {
-#   ami           = "ami-0e35ddab05955cf57"
-#   instance_type = "t3.micro"
-#   subnet_id     = "subnet-0881f59f14c87cbf1"
-#   associate_public_ip_address = false
-
-#   tags = {
-#     Name = "MyTestInstance"
-#   }
-
-#   root_block_device {
-#     volume_size = 12
-#   }
-# }
-
-# when using the .tfvars file
 resource "aws_instance" "ubuntu_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
--- ec2_instance/provider.tf
+++ ec2_instance/provider.tf
@@ -1,9 +1,3 @@
-# provider "aws" {
-#   region     = "ap-south-1"
-# }
-
-
-# when using the .tfvars file
 provider "aws" {
   region = var.region
 }
